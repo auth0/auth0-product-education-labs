1ed2db1b1bf9bdf91f9f93b18146b33a729ddfe4
Bobby Johnson <bobby.johnson@gmail.com>
2020-10-19 09:11:23 -0700

    broke out envvars to not use destructuring

--- apps/front-end-spa-app-unsecured/scripts/url-check.js
+++ apps/front-end-spa-app-unsecured/scripts/url-check.js
@@ -1,5 +1,7 @@
 (function () {
-  const { NODE_ENV, VERCEL_GITHUB_REPO, VERCEL_GITHUB_ORG } = process.env;
+  const NODE_ENV = process.env.NODE_ENV;
+  const VERCEL_GITHUB_ORG = process.env.VERCEL_GITHUB_ORG;
+  const VERCEL_GITHUB_REPO = process.env.VERCEL_GITHUB_REPO;
 
   if (NODE_ENV === "production") {
     const appUrl = `https://${VERCEL_GITHUB_REPO}.${VERCEL_GITHUB_ORG.toLowerCase()}.vercel.app`;
