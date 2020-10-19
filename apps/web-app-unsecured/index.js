e353a45639e87ca0204d035a74701dcae6b16110
Bobby Johnson <bobby.johnson@gmail.com>
2020-10-19 08:41:22 -0700

    added custom handler to redirect to correct url

--- apps/web-app-unsecured/index.js
+++ apps/web-app-unsecured/index.js
@@ -2,12 +2,34 @@ const express = require("express");
 const { createServer } = require("http");
 const morgan = require("morgan");
 
-const { NODE_ENV = "development", PORT = 7000 } = process.env;
+const {
+  NODE_ENV = "development",
+  PORT = 7000,
+  API_URL,
+  SESSION_SECRET,
+  VERCEL_GITHUB_REPO,
+  VERCEL_GITHUB_ORG,
+} = process.env;
 
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.use(morgan("combined"));
+
+let appUrl = `http://localhost:${PORT}`;
+
+if (NODE_ENV === "production") {
+  appUrl = `https://${VERCEL_GITHUB_REPO}.${VERCEL_GITHUB_ORG.toLowerCase()}.vercel.app`;
+
+  app.use((req, res, next) => {
+    const host = req.headers.host;
+    if (!appUrl.includes(host)) {
+      return res.status(301).redirect(appUrl);
+    }
+    return next();
+  });
+}
 
 app.get("/", (req, res) => {
   res.render("home", { user: req.openid && req.openid.user });
