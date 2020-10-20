8767c0321a95a6542aa0647adab2fe8c7e3550ba
Paul Adams <paul@avaloncreative.com>
2020-10-20 13:36:53 -0700

    Corrected port on localhost

--- apps/front-end-spa-app-secured/scripts/navbar.js
+++ apps/front-end-spa-app-secured/scripts/navbar.js
@@ -2,7 +2,7 @@
   const NODE_ENV = process.env.NODE_ENV;
   const VERCEL_GITHUB_ORG = process.env.VERCEL_GITHUB_ORG;
   const VERCEL_GITHUB_REPO = process.env.VERCEL_GITHUB_REPO;
-  let appUrl = "http://localhost:5000";
+  let appUrl = "http://localhost:8000";
 
   if (NODE_ENV === "production") {
     appUrl = `https://${VERCEL_GITHUB_REPO}.${VERCEL_GITHUB_ORG.toLowerCase()}.vercel.app`;
