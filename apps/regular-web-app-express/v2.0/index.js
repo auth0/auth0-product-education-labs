6d54e65148e0373c41bc4310f9aa2538a2a7c23a
Bobby Johnson <bobby.johnson@gmail.com>
2020-11-17 09:20:44 -0800

    adding user profile to error route if available

--- apps/regular-web-app-express/v2.0/index.js
+++ apps/regular-web-app-express/v2.0/index.js
@@ -96,7 +96,9 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", {
+    user: req.oidc && req.oidc.user,
+  });
 });
 
 createServer(app).listen(PORT, () => {
